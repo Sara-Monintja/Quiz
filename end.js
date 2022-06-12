b9ac2b7b0a80f64e1303f4acda4d5c27ebc0c0b3
Sara-M <sara.monintja@gmail.com>
2022-06-13 04:37:56 +1000

    changes9

---
+++ end.js
@@ -0,0 +1,34 @@
+const initials = document.querySelector('#initials')
+const saveScorebtn = document.querySelector('#saveScore')
+const finalScore = document.querySelector('#finalScore')
+const mostRecentScore= document.querySelector('#mostRecentScore')
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+const max_high_scores = 10
+
+finalScore.innerText = mostRecentScore
+
+username.addEventListener('keyup', () => {
+    saveScorebtn.disabled = !initials.value
+})
+
+saveHighScore = e => {
+    e.preventDefault()
+
+    const score = {
+        score : mostRecentScore,
+        name: initials.value
+    }
+
+    highScores,push(score)
+
+    highScores.sort((a,b) => {
+        return b.score - a.score
+    })
+
+    highScores.splice(10)
+
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('/')
+}
