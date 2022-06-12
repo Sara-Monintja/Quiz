059568b7292251c8344f53bda842249167e1120f
Sara-M <sara.monintja@gmail.com>
2022-06-13 03:54:14 +1000

    changes7

--- question.js
+++ question.js
@@ -1,4 +1,4 @@
-const question = document.querySelector('.question');
+const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('.progressText');
 const scoreText = document.querySelector('#progressBarFull');
@@ -13,50 +13,50 @@ let availableQuestions = []
 
 let questions = [
     {
-        title: "Which of the following number object functions returns the value of the number?",
+        question: "Which of the following number object functions returns the value of the number?",
         choices: ["toString()", "toLocaleString()", "valueOf()"],
         answer: "valueOf()"
     },
     {
-        title: "Which of the following is correct about JavaScript?",
+        question: "Which of the following is correct about JavaScript?", 
         choices: ["JavaScript is an Object-oriented language", "JavaScript is Assembly-language", "JavaScript is an Object-Oriented language"],
         answer:"JavaScript is an Object-Oriented language"
     },
     {
-        title: "In JavaScript, what is a block statement?",
+        question: "In JavaScript, what is a block statement?",
         choices: ["Conditional block", "Block that combines a number of statements into a single compund statement","Both conditional block and single statement"],
         answer: "Block that combines a number of statements into a single compund statement"
     },
     {
-        title: "The 'Function' and 'Var' are known as?",
+        question: "The 'Function' and 'Var' are known as?",
         choices: ["Keywords", "Data Types", "Declaration statements"],
         answer: "Declaration statements"
     },
     {
-        title: "Which one of the following is the correct way for calling the JavaScript code",
+        question: "Which one of the following is the correct way for calling the JavaScript code",
         choices: ["Function/method", "RMI", "Trigerring event"],
         answer: "Function/method"
     },
     {
-        title: "What is JavaScript?",
+        question: "What is JavaScript?",
         choices: ["JavaScript is a scripting language used to make the website interactive", "JavaScript is an assembly language used to make the website interactive","JavaScript is a compiles language used to make the website interactive"],
         answer:"JavaScript is a compiles language used to make the website interactive"
     },{
-        title: "Which one of the following symbol is used for creating comments in the JavaScript?",
+        question: "Which one of the following symbol is used for creating comments in the JavaScript?",
         choices: ["//", "\\", "\/"],
         answer: "//"
     },
     {
-        title: "Which of the following operator returns false if both values are equal?",
+        question: "Which of the following operator returns false if both values are equal?",
         choices: ["!", "!=", "!=="],
         answer:"!="
     },{
-        title: "Which one of the following is known as the Equality operator, which is used to check whether two values are equal or not:",
+        question: "Which one of the following is known as the Equality operator, which is used to check whether two values are equal or not:",
         choices: ["&&", "===", "=="],
         answer: "=="
     },
     {
-        title: "Which of the following function of the String object returns the character in the string starting at the specified position via the specified number of characters?",
+        question: "Which of the following function of the String object returns the character in the string starting at the specified position via the specified number of characters?",
         choices: ["slice()", "split()", "substr()"],
         answer:"substr()"
     },
