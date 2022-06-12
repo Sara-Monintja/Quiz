a4a560442bf14344275544868e706211adbed67b
Sara-M <sara.monintja@gmail.com>
2022-06-13 02:03:56 +1000

    changes3

--- question.js
+++ question.js
@@ -1,4 +1,17 @@
-const questions = [
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('#choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#progressBarFull');
+const progressBarFull = document.querySelector('#progressBarFull')
+
+
+let currentQuestion = {};
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
     {
         title: "Which of the following number object functions returns the value of the number?",
         choices: ["toString()", "toLocaleString()", "valueOf()"],
@@ -49,5 +62,72 @@ const questions = [
     },
 ]
 
+const score_points = 100
+const max_questions = 10
 
+startGame = () => {
+    questionCounter = 0
+    score = 0 
+    availableQuestions = [...questions]
+    getNewQuestions()
+}
 
+getNewQuestions = () => {
+    if(availableQuestions.length ===0 || questionCounter > max_questions) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++ 
+    progressText.innerText = `Question ${questionCounter} of ${max-questions}`
+    progressBarFull.style.width = `$(questionCounter/max_questions) * 100}%`
+
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionIndex, 1)
+
+    acceptingAnswers = true
+
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.targetconst 
+        const selectedAnswer = selectedChoice.dataset['number']
+        
+        // if correct / incorrect -- target CSS colour
+        let classToApply = selectedAnswer === currentQuestion.answer ? 'correct' : 'incorrect'
+        
+        // if answer is correct, increase score by 100 
+        if(classToApply === 'correct') {
+            incrementScore(score_points)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        },1000)
+
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
